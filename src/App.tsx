6d46b8b09b42c02d11a5ecb508ee8121d791a9eb
Yash Shrivastava <yash.shrivastava77@gmail.com>
2023-05-14 00:17:18 +0530

    Add page toggle button

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [page, togglePage] = useState<boolean>(true);
   return (
     <>
       <div className="container">
         <h1>Hello there!</h1>
+        <label className="switch">
+          <input type="checkbox" onClick={() => togglePage((page) => !page)} />
+          <span className="slider"></span>
+        </label>
+        {page === true ? <h2>First Page</h2> : <h2>Second Page</h2>}
       </div>
     </>
   );
