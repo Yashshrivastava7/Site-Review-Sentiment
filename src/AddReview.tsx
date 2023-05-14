e0ca9725228a719942f71f5ee9d8117ca3948fb4
Yash Shrivastava <yash.shrivastava77@gmail.com>
2023-05-14 21:36:44 +0530

    Implement AddReview page

---
+++ src/AddReview.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import "./AddReview.css";
+
+type Review = {
+  sitename: string;
+  review: string;
+};
+
+function AddReview() {
+  const [review, setReview] = useState<string>("");
+  const [sitename, setSitename] = useState<string>("");
+  function handleSitename(e: any) {
+    setSitename(e.target.value);
+  }
+  function handleReview(e: any) {
+    setReview(e.target.value);
+  }
+  function handleClick() {
+    const data: Review = {
+      sitename: sitename,
+      review: review,
+    };
+    console.log(data);
+  }
+  return (
+    <>
+      <div className="AddReview">
+        <h1>Add Review</h1>
+        <textarea
+          rows={1}
+          cols={50}
+          className="upperInput"
+          placeholder="Sitename"
+          onChange={handleSitename}
+        />
+        <textarea
+          rows={10}
+          cols={50}
+          placeholder="Review"
+          onChange={handleReview}
+        />
+        <button onClick={handleClick}>Submit</button>
+      </div>
+    </>
+  );
+}
+
+export default AddReview;
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import AddReview from "./AddReview";
 
 function App() {
   const [page, togglePage] = useState<boolean>(true);
@@ -11,7 +12,7 @@ function App() {
           <input type="checkbox" onClick={() => togglePage((page) => !page)} />
           <span className="slider"></span>
         </label>
-        {page === true ? <h2>First Page</h2> : <h2>Second Page</h2>}
+        {page === true ? <AddReview /> : <h2>Second Page</h2>}
       </div>
     </>
   );
